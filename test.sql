d199a12a8094b09b425c4bafcb9c829a12014379
lilyliao <lliao@one-k.com>
2015-11-22 17:51:47 -0800

    load bug

--- test.sql
+++ test.sql
@@ -19,3 +19,4 @@ LOAD xlarge FROM 'xlarge.del' WITH INDEX
 SELECT COUNT(*) FROM xlarge
 SELECT * FROM xlarge WHERE key = 4240
 SELECT * FROM xlarge WHERE key > 400 AND key < 500 AND key > 100 AND key < 4000000
+
